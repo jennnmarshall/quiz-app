2e9b3096969254ff4b96fde4157cab4ee037958a
Jenn Marshall <jennyfever@gmail.com>
2022-11-02 00:28:27 -0600

    second draft of js, fixed indexing, event listeners for list items

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,81 +13,83 @@ var wrong = document.createElement("p");
 var score = 0;
 
 // variable array of question objects
-var questions = [q1, q2];
-// question objects
-var q1 = {
-  question: "What does HTML stand for?",
-  choices: [
-    "Hyper Trainer Markup Language",
-    "Hyper Text Marketing Language",
-    "Hyper Text Markup Language",
-    "Hyena Toothfairy Madeup Lingo",
-  ],
-  answer: "Hyper Text Markup Language",
-};
-
-var q2 = {
+var questions = [
+  {
+    question: "What does HTML stand for?",
+    choices: [
+      "Hyper Trainer Markup Language",
+      "Hyper Text Marketing Language",
+      "Hyper Text Markup Language",
+      "Hyena Toothfairy Madeup Lingo",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
+  {
     question: "<h1>Header</h1> is the correct way of making a header in HTML.",
     choices: ["True", "False", "Red", "Blue"],
     answer: "True",
-}
+  },
+];
 
-questionText.textContent = q1.question;
-qChoice1.textContent = q1.choices[0];
-qChoice2.textContent = q1.choices[1];
-qChoice3.textContent = q1.choices[2];
-qChoice4.textContent = q1.choices[3];
+var index = 0;
+
+
+console.log(questions);
+console.log(questions[0].question);
+
+questionText.textContent = questions[0].question;
+qChoice1.textContent = questions[0].choices[0];
+qChoice2.textContent = questions[0].choices[1];
+qChoice3.textContent = questions[0].choices[2];
+qChoice4.textContent = questions[0].choices[3];
 correct.textContent = "You're right!";
 wrong.textContent = "Wrong!";
 
 // function to change out question
-var changeQuestion = function() {
-    // for (var i = 0; i < questions.length; i++) {
-    //     questionText.textContent = questions[i].question;
-    //     qChoice1.textContent = questions[i].choices[0];
-    //     qChoice2.textContent = questions[i].choices[1];
-    //     qChoice3.textContent = questions[i].choices[2];
-    //     qChoice4.textContent = questions[i].choices[3];
-    // }  
-    questionText.textContent = q2.question;
-    qChoice1.textContent = q2.choices[0];
-    qChoice2.textContent = q2.choices[1];
-    qChoice3.textContent = q2.choices[2];
-    qChoice4.textContent = q2.choices[3];
-    questionBox.appendChild(questionText);
-    questionBox.appendChild(listEl);
-    listEl.appendChild(qChoice1);
-    listEl.appendChild(qChoice2);
-    listEl.appendChild(qChoice3);
-    listEl.appendChild(qChoice4);
+var changeQuestion = function () {
+    if (index < questions.length) {
+    index++;
+    };
+  questionText.textContent = questions[index].question;
+  qChoice1.textContent = questions[index].choices[0];
+  qChoice2.textContent = questions[index].choices[1];
+  qChoice3.textContent = questions[index].choices[2];
+  qChoice4.textContent = questions[index].choices[3];
+  questionBox.appendChild(questionText);
+  questionBox.appendChild(listEl);
+  listEl.appendChild(qChoice1);
+  listEl.appendChild(qChoice2);
+  listEl.appendChild(qChoice3);
+  listEl.appendChild(qChoice4);
+  console.log(score);
 };
 
 startButton.addEventListener("click", function (event) {
   console.log("it works");
   start.setAttribute("style", "display:none;");
-//   start timer
-    body.appendChild(questionBox);
-    questionBox.appendChild(questionText);
-    questionBox.appendChild(listEl);
-    listEl.appendChild(qChoice1);
-    listEl.appendChild(qChoice2);
-    listEl.appendChild(qChoice3);
-    listEl.appendChild(qChoice4);
+  //   start timer
+  body.appendChild(questionBox);
+  questionBox.appendChild(questionText);
+  questionBox.appendChild(listEl);
+  listEl.appendChild(qChoice1);
+  listEl.appendChild(qChoice2);
+  listEl.appendChild(qChoice3);
+  listEl.appendChild(qChoice4);
 });
 
-qChoice1.addEventListener("click", function(event) {
-    if (qChoice1.textContent === q1.answer) {
-        listEl.appendChild(correct);
-        score++;
-    } else {
-        listEl.appendChild(wrong);
-        // time subtract
-    }
-    changeQuestion();
+qChoice1.addEventListener("click", function (event) {
+  if (qChoice1.textContent === questions[index].answer) {
+    listEl.appendChild(correct);
+    score++;
+  } else {
+    listEl.appendChild(wrong);
+    // time subtract
+  }
+  changeQuestion();
 });
 
 qChoice2.addEventListener("click", function (event) {
-  if (qChoice2.textContent === q1.answer) {
+  if (qChoice2.textContent === questions[index].answer) {
     listEl.appendChild(correct);
     score++;
   } else {
@@ -98,7 +100,7 @@ qChoice2.addEventListener("click", function (event) {
 });
 
 qChoice3.addEventListener("click", function (event) {
-  if (qChoice3.textContent === q1.answer) {
+  if (qChoice3.textContent === questions[index].answer) {
     listEl.appendChild(correct);
     score++;
   } else {
@@ -109,7 +111,7 @@ qChoice3.addEventListener("click", function (event) {
 });
 
 qChoice4.addEventListener("click", function (event) {
-  if (qChoice4.textContent === q1.answer) {
+  if (qChoice4.textContent === questions[index].answer) {
     listEl.appendChild(correct);
     score++;
   } else {
@@ -118,6 +120,3 @@ qChoice4.addEventListener("click", function (event) {
   }
   changeQuestion();
 });
-
-console.log(score);
-
