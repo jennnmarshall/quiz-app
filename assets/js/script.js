3703a73ac84816e833527e057061581e50fd09b8
Jenn Marshall <jennyfever@gmail.com>
2022-11-03 17:13:14 -0600

    new code for submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// global variable list
 var startButton = document.querySelector("#start");
 var start = document.querySelector("#start-quiz");
 var body = document.body;
@@ -11,6 +12,19 @@ var qChoice4 = document.createElement("li");
 var correct = document.createElement("p");
 var wrong = document.createElement("p");
 var score = 0;
+var index = 0;
+var timeLeft = 75;
+var timer = document.querySelector("#timer");
+var highScore = document.querySelector("#score");
+var gameOver = document.querySelector("#game-over");
+var gameOverPage = document.querySelector(".last-page");
+var timeInterval = null;
+var initialSubmit = document.querySelector("#submit");
+var initialInput = document.querySelector("#name-init");
+var scoreList = document.createElement("ol");
+// adds class "alt colors" to the score list
+scoreList.classList.add("alt-colors");
+// user array with initials and score
 
 // variable array of question objects
 var questions = [
@@ -31,43 +45,77 @@ var questions = [
   },
 ];
 
-var index = 0;
-
-
-console.log(questions);
-console.log(questions[0].question);
-
+// text content for first question
 questionText.textContent = questions[0].question;
 qChoice1.textContent = questions[0].choices[0];
 qChoice2.textContent = questions[0].choices[1];
 qChoice3.textContent = questions[0].choices[2];
 qChoice4.textContent = questions[0].choices[3];
 correct.textContent = "You're right!";
-wrong.textContent = "Wrong!";
+wrong.textContent = "That's incorrect!";
 
-// function to change out question
+// function for timer
+function startTimer() {
+  timeInterval = setInterval(function () {
+    timeLeft--;
+    timer.textContent = timeLeft + " seconds left";
+    body.appendChild(timer);
+
+    if (timeLeft === 0) {
+      clearInterval(timeInterval);
+      timer.textContent = "Time's up!";
+      body.appendChild(timer);
+    }
+  }, 1000);
+}
+
+// function to stop the timer, called when user finishes before time
+function stopTimer() {
+  clearInterval(timeInterval);
+  timer.textContent = "Time's up!";
+  body.appendChild(timer);
+}
+
+var newScore = score * 5;
+
+
+
+// return user
+// function to change out question, or to go to the end page if out of questions
 var changeQuestion = function () {
-    if (index < questions.length) {
-    index++;
-    };
-  questionText.textContent = questions[index].question;
-  qChoice1.textContent = questions[index].choices[0];
-  qChoice2.textContent = questions[index].choices[1];
-  qChoice3.textContent = questions[index].choices[2];
-  qChoice4.textContent = questions[index].choices[3];
-  questionBox.appendChild(questionText);
-  questionBox.appendChild(listEl);
-  listEl.appendChild(qChoice1);
-  listEl.appendChild(qChoice2);
-  listEl.appendChild(qChoice3);
-  listEl.appendChild(qChoice4);
-  console.log(score);
+  index++;
+  if (index === questions.length) {
+    gameOverPage.setAttribute("style", "display:block");
+    highScore.textContent = "Your score is " + newScore + ", well done!";
+    gameOver.appendChild(highScore);
+    questionText.remove();
+    qChoice1.remove();
+    qChoice2.remove();
+    qChoice3.remove();
+    qChoice4.remove();
+    correct.remove();
+    wrong.remove();
+    stopTimer();
+  } else {
+    questionText.textContent = questions[index].question;
+    qChoice1.textContent = questions[index].choices[0];
+    qChoice2.textContent = questions[index].choices[1];
+    qChoice3.textContent = questions[index].choices[2];
+    qChoice4.textContent = questions[index].choices[3];
+    questionBox.appendChild(questionText);
+    questionBox.appendChild(listEl);
+    listEl.appendChild(qChoice1);
+    listEl.appendChild(qChoice2);
+    listEl.appendChild(qChoice3);
+    listEl.appendChild(qChoice4);
+    console.log(score);
+  }
 };
 
+// start button event listener function
 startButton.addEventListener("click", function (event) {
-  console.log("it works");
-  start.setAttribute("style", "display:none;");
-  //   start timer
+  document.getElementById("start-quiz").style.display = "none";
+  startTimer();
   body.appendChild(questionBox);
   questionBox.appendChild(questionText);
   questionBox.appendChild(listEl);
@@ -83,7 +131,7 @@ qChoice1.addEventListener("click", function (event) {
     score++;
   } else {
     listEl.appendChild(wrong);
-    // time subtract
+    timeLeft = timeLeft - 10;
   }
   changeQuestion();
 });
@@ -94,7 +142,7 @@ qChoice2.addEventListener("click", function (event) {
     score++;
   } else {
     listEl.appendChild(wrong);
-    // time subtract
+    timeLeft = timeLeft - 10;
   }
   changeQuestion();
 });
@@ -105,7 +153,7 @@ qChoice3.addEventListener("click", function (event) {
     score++;
   } else {
     listEl.appendChild(wrong);
-    // time subtract
+    timeLeft = timeLeft - 10;
   }
   changeQuestion();
 });
@@ -116,7 +164,60 @@ qChoice4.addEventListener("click", function (event) {
     score++;
   } else {
     listEl.appendChild(wrong);
-    // time subtract
+    timeLeft = timeLeft - 10;
   }
   changeQuestion();
 });
+// High Scores! title on page 3
+var scoreTitle = document.querySelector("#score-title");
+// unhide page 3 variable
+var highScorePage = document.querySelector(".score-page");
+
+// console.log(user);
+// console.log(initialInput);
+
+// this function gets the item
+function scoreTable(event) {
+  event.preventDefault();
+// userscore is an empty array where the userInfo objects are stored
+    
+  var userScore = JSON.parse(localStorage.getItem("user")) || [];
+//   set item, add displays here
+// userinfo is the object array containing the initials and player score
+  var userInfo = {
+    name: initialInput.value,
+    score: newScore,
+  };
+    userScore.push(userInfo);
+  
+    localStorage.setItem("user", JSON.stringify(userScore));
+    console.log(userScore);
+
+    gameOverPage.setAttribute("style", "display:none");
+    highScorePage.setAttribute("style", "display:block");
+  };
+
+
+// go back button, reset scores button
+initialSubmit.addEventListener("submit", function() {
+    scoreTable();
+    printHighScores();
+}); 
+
+function printHighScores() {
+    event.preventDefault();
+
+    // move get item localstorage
+    userScore.sort(function(a, b) {
+    return b.score - a.score;
+    });
+    for (var i = 0; i < userScore.length; i+=1) {
+    var newLine = document.createElement("li");
+    newLine.textContent = userScore[i].name + " - " + userScore[i].score;
+    scoreTitle.appendChild(scoreList);
+    scoreList.appendChild(newLine);
+    }};
+// function 1 - initial box form with user score array, object user info, push of 
+// user info into user score ==> to localstorage, call next function
+// function 2 - call userscore, sort, forloop
+// new html page for high scores
